fcecf3363a5cc194eeed495f6010a09a9be92324
painworkpc <zhangty@all-help.com>
2017-03-03 10:09:30 +0800

    init

---
+++ mylibrary/src/main/java/com/painmaventest/mylibrary/BaseActyvity.java
@@ -0,0 +1,16 @@
+package com.painmaventest.mylibrary;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhangty on 2017/3/3.
+ */
+
+public class BaseActyvity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
